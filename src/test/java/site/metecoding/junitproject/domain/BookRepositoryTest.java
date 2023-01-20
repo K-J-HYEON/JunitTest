a4495416152cef72368b5ddbea0673554c2f804d
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-20 12:36:39 +0900

    레포지토리 테스트 - 책삭제하기 버그 수정

--- src/test/java/site/metecoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metecoding/junitproject/domain/BookRepositoryTest.java
@@ -1,9 +1,13 @@
 package site.metecoding.junitproject.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
 
 @DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
@@ -11,11 +15,27 @@ public class BookRepositoryTest {
     @Autowired // DI
     private BookRepository bookRepository;
 
+    // @BeforeEach // 테스트 시작전에 한번만 실행
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void 데이터준비() {
+        String title = "junit";
+        String author = "겟인데어";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    } // 트랜잭션 종료 됐다면 말이안됨
+    // 가정 1 : [데이터준비() + 1 책등록 ] (T), [ 데이터준비() + 2 책목록보기 ] (T)
+    // 가정 2 : [데이터준비() + 1 책등록 + 데이터준비() + 2 책목록보기 ] (T)
+
+
+
     // 1. 책 등록
     @Test
     public void 책등록_test() {
         // given (데이터 준비)
-        String title = "junit";
+        String title = "junit5";
         String author = "메타코딩";
         Book book = Book.builder()
                 .title(title)
@@ -31,10 +51,52 @@ public class BookRepositoryTest {
     }
 
     // 2. 책 목록보기
+    @Test
+    public void 책목록보기_test() {
+        // given
+        String title = "junit";
+        String author = "겟인데어";
+
+        // when
+        List<Book> booksPS = bookRepository.findAll();
+
+        System.out.println("사이즈 : ================================= : " + booksPS.size());
+
+        // then
+        assertEquals(title, booksPS.get(0).getTitle());
+        assertEquals(author, booksPS.get(0).getAuthor());
+
+    } // 트랜잭션 종료(저장된 데이터를 초기화함)
 
     // 3. 책 한건보기
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책한건보기_test() {
+        // given
+        String title = "junit";
+        String author = "겟인데어";
 
-    // 4. 책 수정
+        // when
+        Book bookPS = bookRepository.findById(1L).get();
 
-    // 5. 책 삭제
+        // then
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
+
+    // 4. 책 삭제
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책삭제_test() {
+        // given
+        Long id = 1L;
+
+        // when
+        bookRepository.deleteById(id);
+
+        // then
+        assertFalse(bookRepository.findById(id).isPresent());
+    }
+
+    // 5. 책 수정
 }
