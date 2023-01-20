23b8ce867454d195606b4c87c6e5ee7d3575d2a9
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-20 10:30:01 +0900

    "책등록_test 완료"

--- src/test/java/site/metecoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metecoding/junitproject/domain/BookRepositoryTest.java
@@ -1,19 +1,33 @@
 package site.metecoding.junitproject.domain;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
 
-    @Autowired
+    @Autowired // DI
     private BookRepository bookRepository;
 
     // 1. 책 등록
     @Test
     public void 책등록_test() {
-        System.out.println("책등록_test 실행");
+        // given (데이터 준비)
+        String title = "junit";
+        String author = "메타코딩";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        // when (테스트 실행)
+        Book bookPS = bookRepository.save(book);
+
+        // then (검증)
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
     }
 
     // 2. 책 목록보기
